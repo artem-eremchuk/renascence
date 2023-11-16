5f498b3b5a01fd5daf72876701cfa5fd5fcbd270
Artem Eremchuk <eremchuk.it@gmail.com>
2023-11-16 13:16:29 +0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// const burger = document.querySelector(".btn__img");
+
+// burger.addEventListener("click", () => {
+//   burger.src = "assets/images/icons/burger-cross.svg";
+// });
+
+// console.log(burger);
